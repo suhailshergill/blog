34bd7fddd2a5dfd55c1dc32d064a23182614891d
Suhail Shergill <suhailshergill@gmail.com>
2012-03-18 18:38:55 +0000

    adding a utils file to store cross-handler functions. adding a function to get a bofh excuse

---
+++ blog/Utils.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+module Utils
+       ( getBOFHExcuses
+       ) where
+
+import Prelude
+import System.Process (readProcess)
+import Control.Monad.IO.Class
+
+type Title = String
+type Body = String
+getBOFHExcuses :: (MonadIO m) => m (Title,Body)
+getBOFHExcuses = do
+  title:body <- return . filter (/= "") . lines =<< liftIO (readProcess "fortune"
+                                                        ["bofh-excuses"] "")
+  return $! (title, unlines body)
+
+
+-- main = getBOFHExcuses
