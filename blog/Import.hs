e7ef7a2cb46299ec74e77333ea5bc28441a72290
Suhail Shergill <suhailshergill@gmail.com>
2012-06-08 18:00:21 +0000

    Clean up exports
    * blog/Import.hs: Avoid unnecessary repetition by importing everything
      as 'X'

--- blog/Import.hs
+++ blog/Import.hs
@@ -1,49 +1,30 @@
 module Import
-    ( module Prelude
-    , module Yesod
-    , module Foundation
-    , module Settings.StaticFiles
-    , module Data.Monoid
-    , module Control.Applicative
-    , module Data.Text
+    ( module X
 #if __GLASGOW_HASKELL__ < 704
     , (<>)
 #endif
-    , module Yesod.Paginator
-    , module Database.Persist.Query.Join
-    , module Yesod.Default.Config
-    , runJoin
-
-    , module Text.Hamlet
-    , module Safe
-
-    , module Data.Time
-    , module Data.Time.Format.Human
-    , module System.Locale
-
-    , module SqlStmts
     ) where
 
-import Prelude hiding (writeFile, readFile, head, tail, init, last)
-import Yesod   hiding (Route(..))
-import Foundation hiding (Key, Entity, entityKey, entityVal, unKey)
-import Data.Monoid (Monoid (mappend, mempty, mconcat))
-import Control.Applicative ((<$>), (<*>), pure)
-import Data.Text (Text, pack)
-import Settings.StaticFiles
-import Yesod.Paginator
-import Database.Persist.Query.Join hiding (runJoin)
-import Database.Persist.Query.Join.Sql (runJoin)
-import Yesod.Default.Config
+import Prelude as X hiding (writeFile, readFile, head, tail, init, last)
+import Yesod as X hiding (Route(..))
+import Foundation as X hiding (Key, Entity, entityKey, entityVal, unKey)
+import Data.Monoid as X (Monoid (mappend, mempty, mconcat))
+import Control.Applicative as X ((<$>), (<*>), pure)
+import Data.Text as X (Text, pack)
+import Settings.StaticFiles as X
+import Yesod.Paginator as X
+import Database.Persist.Query.Join as X hiding (runJoin)
+import Database.Persist.Query.Join.Sql as X (runJoin)
+import Yesod.Default.Config as X
 
-import Text.Hamlet (hamletFile)
+import Text.Hamlet as X (hamletFile)
 
-import Safe
-import Data.Time (formatTime, getCurrentTime, UTCTime)
-import Data.Time.Format.Human
-import System.Locale (defaultTimeLocale, rfc822DateFormat)
+import Safe as X
+import Data.Time as X (formatTime, getCurrentTime, UTCTime)
+import Data.Time.Format.Human as X
+import System.Locale as X (defaultTimeLocale, rfc822DateFormat)
 
-import SqlStmts
+import SqlStmts as X
 
 #if __GLASGOW_HASKELL__ < 704
 infixr 5 <>
