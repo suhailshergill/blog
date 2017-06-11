b18e0ba9b56741d22afdbbafa59975bac50ce9df
Suhail Shergill <suhailshergill@gmail.com>
2017-06-11 14:19:01 -0400

    dev/hakyll: blob

--- blog/default.nix
+++ blog/default.nix
@@ -1,9 +1,9 @@
 { mkDerivation, base, blaze-html, blaze-markup, bytestring
 , clientsession, conduit, containers, data-default, directory
-, fast-logger, friendly-time, hamlet, hjsmin, http-conduit
+, fast-logger, friendly-time, hakyll, hamlet, hjsmin, http-conduit
 , http-types, monad-control, monad-logger, mtl, old-locale
 , persistent, persistent-postgresql, process, resourcet, safe
-, shakespeare-css, shakespeare-js, shakespeare-text, stdenv
+, shake, shakespeare-css, shakespeare-js, shakespeare-text, stdenv
 , template-haskell, text, time, transformers, wai, wai-extra, warp
 , yaml, yesod, yesod-auth, yesod-core, yesod-default, yesod-form
 , yesod-newsfeed, yesod-paginator, yesod-platform, yesod-static
@@ -17,13 +17,13 @@ mkDerivation {
   isExecutable = true;
   buildDepends = [
     base blaze-html blaze-markup bytestring clientsession conduit
-    containers data-default directory fast-logger friendly-time hamlet
-    hjsmin http-conduit http-types monad-control monad-logger mtl
-    old-locale persistent persistent-postgresql process resourcet safe
-    shakespeare-css shakespeare-js shakespeare-text template-haskell
-    text time transformers wai wai-extra warp yaml yesod yesod-auth
-    yesod-core yesod-default yesod-form yesod-newsfeed yesod-paginator
-    yesod-platform yesod-static yesod-test
+    containers data-default directory fast-logger friendly-time hakyll
+    hamlet hjsmin http-conduit http-types monad-control monad-logger
+    mtl old-locale persistent persistent-postgresql process resourcet
+    safe shake shakespeare-css shakespeare-js shakespeare-text
+    template-haskell text time transformers wai wai-extra warp yaml
+    yesod yesod-auth yesod-core yesod-default yesod-form yesod-newsfeed
+    yesod-paginator yesod-platform yesod-static yesod-test
   ];
   testDepends = [ base yesod-test ];
   homepage = "http://blog.shergill.su/";
